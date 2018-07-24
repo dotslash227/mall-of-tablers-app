8c84279865d8ea66ef1824ecdc38a7772d29a471
Sanyal <nilabjo.sanyal@accenture.com>
2018-07-24 13:35:25 +0530

    search api integrated

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { FormGroup, FormControl } from '@angular/forms';
 import { SearchProvider } from "../../providers/search/search";
+import { ProfileModalComponent } from '../../components/profile-modal/profile-modal';
+import { MemberListPage } from '../member-list/member-list';
 
 /**
  * Generated class for the SearchPage page.
@@ -20,7 +22,13 @@ export class SearchPage {
     searchTerm: new FormControl()
   })
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private searchProvider: SearchProvider) {
+  noCategoriesFound: boolean = false;
+  noUsersFound: boolean = false;
+  showList: boolean = false;
+  categoriesRes;
+  usersRes;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private searchProvider: SearchProvider, private modalCtrl: ModalController) {
   }
 
   ionViewDidLoad() {
@@ -39,8 +47,30 @@ export class SearchPage {
 
     this.searchProvider.searchFor(searchTerm)
     .subscribe((res) => {
+      this.noCategoriesFound = false;
+      this.noUsersFound = false;
+
       console.log(res);
+
+      this.categoriesRes = res['categories'];
+      this.usersRes = res['users'];
+      this.showList = true;
+      if (this.categoriesRes.length == 0) {
+        this.noCategoriesFound = true;
+      }
+      if (this.usersRes.length == 0) {
+        this.noUsersFound = true;
+      }
     }, error => console.log(error))
+  }
+
+  openCategory(id) {
+    this.navCtrl.push(MemberListPage, {businessId: id});
+  }
+
+  openProfileModal(details) {
+    let profileModal = this.modalCtrl.create(ProfileModalComponent, {memberDetails: details});
+    profileModal.present();
   }
 
 }
